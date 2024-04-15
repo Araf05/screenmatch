b99e0c782417ffce9e4855368b46d88e25aade63
Araceli Noemí Fernández <aracelifernandez.3006@gmail.com>
2024-04-14 22:15:22 -0300

    Refactoriza proyecto, crea clase Titulo y Serie

--- src/io/araf/screenmatch/modelos/Pelicula.java
+++ src/io/araf/screenmatch/modelos/Pelicula.java
@@ -1,68 +1,13 @@
 package io.araf.screenmatch.modelos;
 
-public class Pelicula {
-    private String nombre;
-    private int fechaDeLanzamiento;
-    private int duracionEnMinutos;
-    private boolean incluidoEnPlan;
-    private double sumaDeLasEvaluaciones;
-    private int cantidadDeEvaluaciones;
+public class Pelicula extends Titulo {
+    private String director;
 
-    public int getFechaDeLanzamiento() {
-        return fechaDeLanzamiento;
+    public String getDirector() {
+        return director;
     }
 
-    public void setFechaDeLanzamiento(int fechaDeLanzamiento) {
-        this.fechaDeLanzamiento = fechaDeLanzamiento;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public int getDuracionEnMinutos() {
-        return duracionEnMinutos;
-    }
-
-    public void setDuracionEnMinutos(int duracionEnMinutos) {
-        this.duracionEnMinutos = duracionEnMinutos;
-    }
-
-    public boolean isIncluidoEnPlan() {
-        return incluidoEnPlan;
-    }
-
-    public void setIncluidoEnPlan(boolean incluidoEnPlan) {
-        this.incluidoEnPlan = incluidoEnPlan;
-    }
-
-    public double getSumaDeLasEvaluaciones() {
-        return sumaDeLasEvaluaciones;
-    }
-
-    public int getCantidadDeEvaluaciones() {
-        return cantidadDeEvaluaciones;
-    }
-
-    public void muestraFichaTecnica() {
-        String fichaTecnica = """
-                Película: %s
-                Fecha de lanzamiento: %d
-                Duración: %dmin
-                """.formatted(nombre, fechaDeLanzamiento, duracionEnMinutos);
-        System.out.println(fichaTecnica);
-    }
-
-    public void evalua(double nota) {
-        sumaDeLasEvaluaciones += nota;
-        cantidadDeEvaluaciones++;
-    }
-
-    public double calculaMedia() {
-        return sumaDeLasEvaluaciones / cantidadDeEvaluaciones;
+    public void setDirector(String director) {
+        this.director = director;
     }
 }
---
+++ src/io/araf/screenmatch/modelos/Titulo.java
@@ -0,0 +1,68 @@
+package io.araf.screenmatch.modelos;
+
+public class Titulo {
+    private String nombre;
+    private int fechaDeLanzamiento;
+    private int duracionEnMinutos;
+    private boolean incluidoEnPlan;
+    private double sumaDeLasEvaluaciones;
+    private int cantidadDeEvaluaciones;
+
+    public int getFechaDeLanzamiento() {
+        return fechaDeLanzamiento;
+    }
+
+    public void setFechaDeLanzamiento(int fechaDeLanzamiento) {
+        this.fechaDeLanzamiento = fechaDeLanzamiento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getDuracionEnMinutos() {
+        return duracionEnMinutos;
+    }
+
+    public void setDuracionEnMinutos(int duracionEnMinutos) {
+        this.duracionEnMinutos = duracionEnMinutos;
+    }
+
+    public boolean isIncluidoEnPlan() {
+        return incluidoEnPlan;
+    }
+
+    public void setIncluidoEnPlan(boolean incluidoEnPlan) {
+        this.incluidoEnPlan = incluidoEnPlan;
+    }
+
+    public double getSumaDeLasEvaluaciones() {
+        return sumaDeLasEvaluaciones;
+    }
+
+    public int getCantidadDeEvaluaciones() {
+        return cantidadDeEvaluaciones;
+    }
+
+    public void muestraFichaTecnica() {
+        String fichaTecnica = """
+                Película: %s
+                Fecha de lanzamiento: %d
+                Duración: %dmin
+                """.formatted(nombre, fechaDeLanzamiento, duracionEnMinutos);
+        System.out.println(fichaTecnica);
+    }
+
+    public void evalua(double nota) {
+        sumaDeLasEvaluaciones += nota;
+        cantidadDeEvaluaciones++;
+    }
+
+    public double calculaMedia() {
+        return sumaDeLasEvaluaciones / cantidadDeEvaluaciones;
+    }
+}
