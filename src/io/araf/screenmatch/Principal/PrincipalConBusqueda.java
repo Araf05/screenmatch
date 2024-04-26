faa688aedf4367bb9ba8d7f48ad7c2dce91a204f
Araceli Noemí Fernández <aracelifernandez.3006@gmail.com>
2024-04-26 00:25:14 -0300

    Crea clase PrincipalConBusqueda, dependencias Gson, OMDb API

---
+++ src/io/araf/screenmatch/Principal/PrincipalConBusqueda.java
@@ -0,0 +1,37 @@
+package io.araf.screenmatch.Principal;
+
+import com.google.gson.Gson;
+import io.araf.screenmatch.modelos.Titulo;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class PrincipalConBusqueda {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner lectura = new Scanner(System.in);
+        System.out.println("Escriba el nombre de una pelicula: ");
+        var busqueda = lectura.nextLine();
+
+        String direccion = "https://www.omdbapi.com/?t=" + busqueda + "&apikey=8d725995";
+
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(direccion))
+                .build();
+
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+        String json = response.body();
+        System.out.println(json);
+
+        Gson gson = new Gson();
+        Titulo miTitulo = gson.fromJson(json, Titulo.class);
+        System.out.println(miTitulo);
+    }
+}
--- src/io/araf/screenmatch/modelos/Titulo.java
+++ src/io/araf/screenmatch/modelos/Titulo.java
@@ -1,7 +1,11 @@
 package io.araf.screenmatch.modelos;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Titulo implements Comparable<Titulo>{
+    @SerializedName("Title")
     private String nombre;
+    @SerializedName("Year")
     private int fechaDeLanzamiento;
     private int duracionEnMinutos;
     private boolean incluidoEnPlan;
@@ -73,5 +77,11 @@ public class Titulo implements Comparable<Titulo>{
     @Override
     public int compareTo(Titulo otroTitulo) {
         return this.getNombre().compareTo(otroTitulo.getNombre());
+    }
+
+    @Override
+    public String toString() {
+        return  "nombre='" + nombre + '\'' +
+                ", fechaDeLanzamiento=" + fechaDeLanzamiento;
     }
 }
